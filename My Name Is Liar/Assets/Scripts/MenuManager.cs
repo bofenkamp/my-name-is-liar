e99ecadf86afec5f272716f7fd2ad42742c3b3c6
Dmcdominic <dmcdominic@gmail.com>
2018-01-28 05:28:24 -0500

    Main Menu Development

---
+++ My Name Is Liar/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	public void StartGame() {
+		//TODO
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
