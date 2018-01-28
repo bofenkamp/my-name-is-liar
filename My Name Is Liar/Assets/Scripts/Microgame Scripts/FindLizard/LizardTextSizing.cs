47021ba9e85d22ff85caf088fd36289716b5ab10
Dmcdominic <dmcdominic@gmail.com>
2018-01-27 20:34:21 -0500

    Lizard Minigame Text Resizing

---
+++ My Name Is Liar/Assets/Scripts/Microgame Scripts/FindLizard/LizardTextSizing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LizardTextSizing : MonoBehaviour {
+
+	public Text[] TextBoxes;
+
+	private bool adjusted;
+
+	void Update () {
+		if (adjusted) {
+			return;
+		}
+
+		// Check if text has resized to best fit
+		foreach (Text textBox in TextBoxes) {
+			int size = textBox.cachedTextGenerator.fontSizeUsedForBestFit;
+			if (size != 0) {
+				adjustAll ();
+				return;
+			}
+		}
+	}
+
+	void adjustAll() {
+		// Find minimum size
+		int minSize = int.MaxValue;
+		foreach (Text textBox in TextBoxes) {
+			int size = textBox.cachedTextGenerator.fontSizeUsedForBestFit;
+			if (size < minSize) {
+				minSize = size;
+			}
+		}
+
+		// Set all to minimum size
+		foreach (Text textBox in TextBoxes) {
+			textBox.fontSize = minSize;
+			textBox.resizeTextForBestFit = false;
+		}
+
+		adjusted = true;
+	}
+}
